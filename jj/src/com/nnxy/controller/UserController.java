b0d3cef0849ac804446ee586bab498f3af633d0d
liujiajie2018 <74592295+liujiajie2018@users.noreply.github.com>
2020-12-06 12:07:14 +0800

    Update UserController.java

--- jj/src/com/nnxy/controller/UserController.java
+++ jj/src/com/nnxy/controller/UserController.java
@@ -1,22 +1,5 @@
 package com.nnxy.controller;
 
 public interface UserController {	
-  private int id;
-	private String username;
-	private String password;
-	private int sex;
-	private String love;
-	private int id;
-	private String username;
-	private String password;
-	private int sex;
-	private String love;
-  public UserController(int id, String username, int sex, String love) {
-		super();
-	    this.id=id;
-	    this.username=username;
-	    this.password=password;
-	    this.sex=sex;
-	    this.love=love;
-	}
+
 }
